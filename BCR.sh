29c587bfc67371146e9151c623562f239c4314b4
Devang Thakkar <DevangThakkar@users.noreply.github.com>
2019-12-04 18:48:51 -0500

    remove useless arguments

--- BCR.sh
+++ BCR.sh
@@ -3,57 +3,53 @@
 
 sample=$1
 
-bam_dir=$2
-
-result_dir="$3"/"$1"
 mkdir -p $result_dir
 
-threads=$4
+threads=$2
 
 # get index
 #samtools index -@ $threads "$sample".bam
 
 # get unmapped reads
-samtools view -b -h -f 4 -@ $threads "$bam_dir"/"$sample".bam > "$result_dir"/"$sample".unmapped.bam
+samtools view -b -h -f 4 -@ $threads "$sample".bam > "$sample".unmapped.bam
 
 # get IGH reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr14:105586437-106879844" > "$result_dir"/"$sample".IGH.bam
+samtools view -b -h -@ $threads "$sample".bam "chr14:105586437-106879844" > "$sample".IGH.bam
 
 # get IGK reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr2:88857361-90235368" > "$result_dir"/"$sample".IGK.bam
+samtools view -b -h -@ $threads "$sample".bam "chr2:88857361-90235368" > "$sample".IGK.bam
 
 # get IGL reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr22:22026076-22922913" > "$result_dir"/"$sample".IGL.bam
+samtools view -b -h -@ $threads "$sample".bam "chr22:22026076-22922913" > "$sample".IGL.bam
 
 # merge IG reads
-samtools merge -f -@ $threads "$result_dir"/"$sample".IG.bam "$result_dir"/"$sample".IGH.bam "$result_dir"/"$sample".IGK.bam "$result_dir"/"$sample".IGL.bam
+samtools merge -f -@ $threads "$sample".IG.bam "$sample".IGH.bam "$sample".IGK.bam "$sample".IGL.bam
 
 # sort IG reads
-samtools sort -@ $threads -n -o "$result_dir"/"$sample".IG.sorted.bam "$result_dir"/"$sample".IG.bam
+samtools sort -@ $threads -n -o "$sample".IG.sorted.bam "$sample".IG.bam
 
 # sort unmapped reads
-samtools sort -@ $threads -n -o "$result_dir"/"$sample".unmapped.sorted.bam "$result_dir"/"$sample".unmapped.bam
+samtools sort -@ $threads -n -o "$sample".unmapped.sorted.bam "$sample".unmapped.bam
 
 # get unmapped fastq
-bedtools bamtofastq -i "$result_dir"/"$sample".unmapped.sorted.bam -fq "$result_dir"/"$sample".unmapped.R1.fastq -fq2 "$result_dir"/"$sample".unmapped.R2.fastq
+bedtools bamtofastq -i "$sample".unmapped.sorted.bam -fq "$sample".unmapped.R1.fastq -fq2 "$sample".unmapped.R2.fastq
 
 # get IG fastq
-bedtools bamtofastq -i "$result_dir"/"$sample".IG.sorted.bam -fq "$result_dir"/"$sample".IG.R1.fastq -fq2 "$result_dir"/"$sample".IG.R2.fastq
+bedtools bamtofastq -i "$sample".IG.sorted.bam -fq "$sample".IG.R1.fastq -fq2 "$sample".IG.R2.fastq
 
 # get combined fastq
-cat "$result_dir"/"$sample".unmapped.R1.fastq "$result_dir"/"$sample".IG.R1.fastq > "$result_dir"/"$sample".combined.IG.R1.fastq
-cat "$result_dir"/"$sample".unmapped.R2.fastq "$result_dir"/"$sample".IG.R2.fastq > "$result_dir"/"$sample".combined.IG.R2.fastq
+cat "$sample".unmapped.R1.fastq "$sample".IG.R1.fastq > "$sample".combined.IG.R1.fastq
+cat "$sample".unmapped.R2.fastq "$sample".IG.R2.fastq > "$sample".combined.IG.R2.fastq
 
 # mixcr align
-mixcr align -f -r "$result_dir"/"$sample".report -s human -t $threads -p rna-seq -OallowPartialAlignments=true -OvParameters.geneFeatureToAlign=VGeneWithP "$result_dir"/"$sample".combined.IG.R1.fastq "$result_dir"/"$sample".combined.IG.R2.fastq "$result_dir"/"$sample".IG.vdjca
+mixcr align -f -r "$sample".report -s human -t $threads -p rna-seq -OallowPartialAlignments=true -OvParameters.geneFeatureToAlign=VGeneWithP "$sample".combined.IG.R1.fastq "$sample".combined.IG.R2.fastq "$sample".IG.vdjca
 
 # mixcr assemblePartial
-mixcr assemblePartial -f "$result_dir"/"$sample".IG.vdjca "$result_dir"/"$sample".IG.rescued.vdjca
-mixcr assemblePartial -f "$result_dir"/"$sample".IG.rescued.vdjca "$result_dir"/"$sample".IG.rescued2.vdjca
+mixcr assemblePartial -f "$sample".IG.vdjca "$sample".IG.rescued.vdjca
+mixcr assemblePartial -f "$sample".IG.rescued.vdjca "$sample".IG.rescued2.vdjca
 
 # mixcr assemble
-mixcr assemble -f "$result_dir"/"$sample".IG.rescued2.vdjca "$result_dir"/"$sample".IG.clns
+mixcr assemble -f "$sample".IG.rescued2.vdjca "$sample".IG.clns
 
 # mixcr exportClones
-mixcr exportClones -f -o -t "$result_dir"/"$sample".IG.clns "$result_dir"/"$sample".IG.clones.tsv
-
+mixcr exportClones -f -o -t "$sample".IG.clns "$sample".IG.clones.tsv
