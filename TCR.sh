4b8cb759bf86bc7a9cce3cdc7d2678fdbd9f4682
Devang Thakkar <DevangThakkar@users.noreply.github.com>
2019-12-04 18:55:53 -0500

    remove useless arguments

--- TCR.sh
+++ TCR.sh
@@ -2,61 +2,54 @@
 # get mates somehow and make into paired end fastq
 
 sample=$1
-
-bam_dir=$2
-
-result_dir="$3"/"$1"
-mkdir -p $result_dir
-
-threads=$4
+threads=$2
 
 # get index
 #samtools index -@ $threads "$sample".bam
 
 # get unmapped reads
-#samtools view -b -h -f 4 -@ $threads "$bam_dir"/"$sample".bam > "$result_dir"/"$sample".unmapped.bam
+samtools view -b -h -f 4 -@ $threads "$sample".bam > "$sample".unmapped.bam
 
 # get TRA reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr14:21621904-22552132" > "$result_dir"/"$sample".TRA.bam
+samtools view -b -h -@ $threads "$sample".bam "chr14:21621904-22552132" > "$sample".TRA.bam
 
 # get TRB reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr7:142299011-142813287" > "$result_dir"/"$sample".TRB.bam
+samtools view -b -h -@ $threads "$sample".bam "chr7:142299011-142813287" > "$sample".TRB.bam
 
 # get TRG reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr7:38240024-38368055" > "$result_dir"/"$sample".TRG.bam
+samtools view -b -h -@ $threads "$sample".bam "chr7:38240024-38368055" > "$sample".TRG.bam
 
 # get TRD reads
-samtools view -b -h -@ $threads "$bam_dir"/"$sample".bam "chr14:22422546-22466577" > "$result_dir"/"$sample".TRD.bam
+samtools view -b -h -@ $threads "$sample".bam "chr14:22422546-22466577" > "$sample".TRD.bam
 
 # merge TR reads
-samtools merge -f -@ $threads "$result_dir"/"$sample".TR.bam "$result_dir"/"$sample".TRA.bam "$result_dir"/"$sample".TRB.bam "$result_dir"/"$sample".TRG.bam "$result_dir"/"$sample".TRD.bam
+samtools merge -f -@ $threads "$sample".TR.bam "$sample".TRA.bam "$sample".TRB.bam "$sample".TRG.bam "$sample".TRD.bam
 
 # sort TR reads
-samtools sort -@ $threads -n -o "$result_dir"/"$sample".TR.sorted.bam "$result_dir"/"$sample".TR.bam
+samtools sort -@ $threads -n -o "$sample".TR.sorted.bam "$sample".TR.bam
 
 # sort unmapped reads
-#samtools sort -n -o "$result_dir"/"$sample".unmapped.sorted.bam "$result_dir"/"$sample".unmapped.bam
+samtools sort -n -o "$sample".unmapped.sorted.bam "$sample".unmapped.bam
 
 # get unmapped fastq
-#bedtools bamtofastq -i "$result_dir"/"$sample".unmapped.sorted.bam -fq "$result_dir"/"$sample".unmapped.R1.fastq -fq2 "$result_dir"/"$sample".unmapped.R2.fastq
+bedtools bamtofastq -i "$sample".unmapped.sorted.bam -fq "$sample".unmapped.R1.fastq -fq2 "$sample".unmapped.R2.fastq
 
 # get TR fastq
-bedtools bamtofastq -i "$result_dir"/"$sample".TR.sorted.bam -fq "$result_dir"/"$sample".TR.R1.fastq -fq2 "$result_dir"/"$sample".TR.R2.fastq
+bedtools bamtofastq -i "$sample".TR.sorted.bam -fq "$sample".TR.R1.fastq -fq2 "$sample".TR.R2.fastq
 
 # get combined fastq
-cat "$result_dir"/"$sample".unmapped.R1.fastq "$result_dir"/"$sample".TR.R1.fastq > "$result_dir"/"$sample".combined.T.R1.fastq
-cat "$result_dir"/"$sample".unmapped.R2.fastq "$result_dir"/"$sample".TR.R2.fastq > "$result_dir"/"$sample".combined.T.R2.fastq
+cat "$sample".unmapped.R1.fastq "$sample".TR.R1.fastq > "$sample".combined.T.R1.fastq
+cat "$sample".unmapped.R2.fastq "$sample".TR.R2.fastq > "$sample".combined.T.R2.fastq
 
 # mixcr align
-mixcr align -f -r "$result_dir"/"$sample".report -s human -t $threads -p rna-seq -OallowPartialAlignments=true -OvParameters.geneFeatureToAlign=VGeneWithP "$result_dir"/"$sample".combined.T.R1.fastq "$result_dir"/"$sample".combined.T.R2.fastq "$result_dir"/"$sample".T.vdjca
+mixcr align -f -r "$sample".report -s human -t $threads -p rna-seq -OallowPartialAlignments=true -OvParameters.geneFeatureToAlign=VGeneWithP "$sample".combined.T.R1.fastq "$sample".combined.T.R2.fastq "$sample".T.vdjca
 
 # mixcr assemblePartial
-mixcr assemblePartial -f "$result_dir"/"$sample".T.vdjca "$result_dir"/"$sample".T.rescued.vdjca
-mixcr assemblePartial -f "$result_dir"/"$sample".T.rescued.vdjca "$result_dir"/"$sample".T.rescued2.vdjca
+mixcr assemblePartial -f "$sample".T.vdjca "$sample".T.rescued.vdjca
+mixcr assemblePartial -f "$sample".T.rescued.vdjca "$sample".T.rescued2.vdjca
 
 # mixcr assemble
-mixcr assemble -f "$result_dir"/"$sample".T.rescued2.vdjca "$result_dir"/"$sample".T.clns
+mixcr assemble -f "$sample".T.rescued2.vdjca "$sample".T.clns
 
 # mixcr exportClones
-mixcr exportClones -f -o -t "$result_dir"/"$sample".T.clns "$result_dir"/"$sample".T.clones.tsv
-
+mixcr exportClones -f -o -t "$sample".T.clns "$sample".T.clones.tsv
